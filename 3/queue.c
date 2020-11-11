24c5f8d626471c4eac341fdf13fa5b0b3dcbb578
Sajan Toor <Sajantoor98@gmail.com>
2020-11-11 12:17:24 -0800

    :sparkles: You weren't supposed to do it with linked lists :(

--- 3/queue.c
+++ 3/queue.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "queue.h"
+#define QUEUE = 1;
 
 
 ///////////////////////////////////////
@@ -18,9 +19,19 @@ queue_t* queue_create() {
     return NULL;
   }
 
-  // sets head and tail
-  queue->head = NULL; 
-  queue->tail = NULL;
+  queue->arr = (int *) malloc(sizeof(int) * 5); // intialize array with size of 5
+  if (queue->arr == NULL) {
+    return NULL;
+  }
+
+  // init head and tail 
+  queue->head = 0; 
+  queue->tail = 0;
+  queue->size = 5;
+  #ifdef QUEUE
+    printf("Created queue successfully! \n");
+  #endif
+
   return queue;
 }
 
@@ -35,26 +46,82 @@ queue_t* enqueue(queue_t* q, int item) {
     return NULL;
   }
 
-  // create new node
-  struct Node * node = (struct Node*) malloc(sizeof(struct Node));
-  // if OS cannot allocate memory
-  if (node == NULL) {
-    return NULL;
-  }
+  // add element to array
+  // move tail++ if not end of array
+  // tail is circular is head is greater than 0 and tail is equal to the size of the array
+  // Expanded if tail == head -> gotta move everything from head -> end of array -> tail 
+      // head = 0, tail is the last element in the array + 1 -> the enqueued element
   
-  node->data = item; 
-  node->next = NULL;
+  // regular adding of items 
+  // q->tail == 0 because if empty both gonna be zero so need to fix that
+  if (q->tail != (q->size -1) && (q->tail != q->head || q->tail == 0)) {  
+    q->arr[q->tail] = item; 
+    #ifdef QUEUE
+     printf("Added item (%d) to array normally at ar[%d]!\n", item, q->tail);
+    #endif
+    q->tail++;
+  // circular adding of items 
+  } else if (q->tail == (q->size - 1) && q->head > 0) {
+    q->arr[q->tail] = item; 
+    q->tail = 0;
+  // expand array and copy 
+  } else if (q->tail == (q->size -1) || q->tail == q->head) {
+    // create new array 
+    int* newArray = (int*) malloc(sizeof(int) * q->size*2); 
+    // OS cannot allocate memory for new array
+    if (newArray == NULL) {
+      queue_free(q);
+      return NULL; 
+    }
 
-  // if linked list is empty, then must add to head as well 
-  if (q->head == NULL) {
-    q->head = node;
-    q->tail = node;
-    return q;
+    #ifdef QUEUE
+     printf("Reallocate successful!\n");
+    #endif
+    
+    int head = q->head; 
+    int tail = q->tail; 
+    int size = q->size;
+    int i = 0; 
+    // copies all items from original array to new array 
+    do {
+      newArray[i] = q->arr[head];
+
+      #ifdef QUEUE
+        printf("Copied item(%d)(%d) to ar[%d] \n", q->arr[head], head, i);
+      #endif
+      // run through array normally 
+      if (head != size - 1) {
+        head++;
+      // go to beginning of array and copy those items 
+      } else {
+        head = 0;
+      }
+
+      i++;
+    } while(head != tail);
+
+    // set queue struct to new array 
+    q->size *= 2; 
+    q->head = 0; 
+    q->tail = i + 1; 
+    // free old array -> fix memory leak 
+    free(q->arr);
+    // set pointer to new array 
+    q->arr = newArray; 
+
+    // add item
+    #ifdef QUEUE
+      printf("Added item (%d) to array expansively at ar[%d]!\n", item, q->tail);
+    #endif
+
+    q->arr[q->tail] = item; 
+    q->tail++; 
+  } else {
+    #ifdef QUEUE
+      printf("I didn't enter anything \n");
+    #endif
   }
 
-  // link the list!
-  q->tail->next = node;
-  q->tail = q->tail->next;
   return q;
 }
 
@@ -71,14 +138,14 @@ int dequeue(queue_t* q) {
     return 0;
   }
   
-  int value = q->head->data;
-  struct Node* prevHead = q->head;  
-  q->head = q->head->next;
+  // int value = q->head->data;
+  // struct Node* prevHead = q->head;  
+  // q->head = q->head->next;
 
-  free(prevHead);
-  prevHead = NULL;  
+  // free(prevHead);
+  // prevHead = NULL;  
 
-  return value;
+  return 0;
 }
 
 // checks if the queue is empty
@@ -87,9 +154,10 @@ bool queue_is_empty(queue_t* q) {
     return true;
   }
 
-  if (q->head == NULL) {
+  // empty if BOTH head and tail are 0
+  if (q->head == 0 && q->tail == 0) {
     return true;
-  }
+  } 
 
   return false;
 }
@@ -104,12 +172,12 @@ int queue_length(queue_t* q) {
 
   unsigned int length = 0;
 
-  struct Node* node = q->head;
+  // struct Node* node = q->head;
 
-  while (node != NULL) {
-    length++;
-    node = node->next;
-  }
+  // while (node != NULL) {
+  //   length++;
+  //   node = node->next;
+  // }
 
   return length;  
 }
@@ -117,27 +185,14 @@ int queue_length(queue_t* q) {
 // frees the queue
 // if q==NULL, returns -1
 void queue_free(queue_t* q) {
+  // don't need to validate q->arr since in the implementation if arr is NULL returns NULL 
   if (q == NULL) {
     return; 
   }
   
-  // need 2 pointers 
-  struct Node* node = q->head;
-  struct Node* prevNode = q->head;
-
-  // free all nodes in queue
-  while (node != NULL) {
-    node = node->next;
-    prevNode->next = NULL;
-    // printf("Freeing node with value %d \n", prevNode->data);
-    free(prevNode);
-    prevNode = NULL;
-    prevNode = node;
-  }
-  
-  // free queue 
-  q->head = NULL;
-  q->tail = NULL;
-  free(q);
-  q = NULL;
+  // free array in queue 
+  free(q->arr); 
+  q->arr = NULL; 
+  free(q); 
+  q = NULL; 
 }
